7bc04e54d4da02980223f0762762569636e90f8f
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2016-10-14 16:26:05 -0500

    add credentials

---
+++ jboard/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  jboard
+//
+//  Created by Luis Ezcurdia on 10/14/16.
+//  Copyright © 2016 Luis Ezcurdia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
