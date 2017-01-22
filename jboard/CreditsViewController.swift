de3dcccbc5b950fa922eac76f8b4ecbe36d0321b
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2017-01-21 18:57:05 -0600

    add navigation controller to credits page

--- jboard/CreditsViewController.swift
+++ jboard/CreditsViewController.swift
@@ -14,17 +14,23 @@ class CreditsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Rightmonkey"
+        loadMarkdown()
+    }
+
+    func loadMarkdown() {
         let filepath = Bundle.main.path(forResource: "credits", ofType: "md")!
         let markdown = try! String(contentsOfFile: filepath)
         let downView = try! DownView(frame: self.view.bounds, markdownString: markdown)
+        downView.isOpaque = false
+        downView.backgroundColor = Theme.Colors.foreground.color
         self.view.addSubview(downView)
         downView.snp.makeConstraints { make in
-            make.top.equalTo(self.view.snp.top).offset(20)
+            make.top.equalTo(self.view.snp.top)
             make.left.equalTo(self.view.snp.left)
             make.right.equalTo(self.view.snp.right)
             make.bottom.equalTo(self.view.snp.bottom)
         }
-
     }
 
 }
